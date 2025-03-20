8373510624d0a399771db0d4b9621f4a58514800
João Vaccari <jvvaccari1@gmail.com>
2025-03-19 22:41:02 -0300

    Add the adminMenu and the modal

--- src/components/ContentCard/ContentCard.tsx
+++ src/components/ContentCard/ContentCard.tsx
@@ -1,4 +1,13 @@
-import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Dialog,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import defaultCardImage from "../../assets/img/defaultCardImage.svg";
@@ -6,9 +15,14 @@ import { ICardData } from "../../interfaces/ICardData";
 import { useAuth } from "../../hooks/useAuth";
 import options from "../../assets/img/ContentCard/options.png";
 
+const adminMenu = ["Editar", "Excluir"];
+
 export function ContentCard({ id, name, teacherInfo, coverImage }: ICardData) {
   const { user } = useAuth();
   const [imageSrc, setImageSrc] = useState<string>(defaultCardImage);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [openProfileModal, setOpenProfileModal] = useState(false);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -20,122 +34,157 @@ export function ContentCard({ id, name, teacherInfo, coverImage }: ICardData) {
     }
   }, [coverImage]);
 
-  const handleOpenLessons = (id: string) => {
+  const handleOpenLessons = () => {
     navigate(`${location.pathname}/${id}`);
   };
 
-  console.log(user);
+  const handleOpenMenu = (e: React.MouseEvent<HTMLImageElement>) => {
+    e.stopPropagation();
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMenuClick = (option: string) => {
+    if (option === "Editar") {
+      setOpenProfileModal(true);
+    } else if (option === "Excluir") {
+      console.log("Excluir");
+    }
+    handleCloseMenu();
+  };
 
   return (
-    <Card
-      sx={{
-        display: "flex",
-        marginY: 2.8,
-        borderRadius: 2,
-        "&:hover": { transform: "scale(1.01)" },
-        cursor: "pointer",
-      }}
-      onClick={() => handleOpenLessons(id)}
-    >
-      <Box
+    <>
+      <Card
         sx={{
           display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          minWidth: "116px",
-          overflow: "hidden",
+          marginY: 2.8,
+          borderRadius: 2,
+          "&:hover": { transform: "scale(1.01)" },
+          cursor: "pointer",
         }}
+        onClick={handleOpenLessons}
       >
-        <CardMedia
-          component="img"
+        <Box
           sx={{
-            width: "76px",
-            height: "58px",
-            aspectRatio: 4 / 3,
-            objectFit: "cover",
-            borderRadius: 1,
-            border: "1px outset whitesmoke",
-            filter: "drop-shadow(0px 0.6px 0.6px rgba(0, 0, 0, 0.7))",
-          }}
-          image={imageSrc}
-          alt="Capa do curso"
-        />
-      </Box>
-
-      <Box
-        sx={{
-          marginLeft: -1.8,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          width: "100%",
-        }}
-      >
-        <CardContent
-          sx={{
-            textAlign: "left",
             display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
+            alignItems: "center",
+            justifyContent: "center",
+            minWidth: "116px",
+            overflow: "hidden",
           }}
         >
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <Typography
-              component="div"
-              sx={{
-                fontSize: { xs: "15px", sm: "16px", md: "18px", lg: "20px" },
-                fontWeight: 600,
-                maxWidth: { xs: "180px", sm: "190px", md: "200px", lg: "100%" },
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-            >
-              {name}
-            </Typography>
-            <Typography
-              variant="subtitle2"
-              component="div"
-              sx={{
-                color: "text.secondary",
-                fontSize: { xs: "12px", md: "14px", lg: "16px" },
-                maxWidth: {
-                  xs: "140px",
-                  sm: "380px",
-                  md: "500px",
-                  lg: "540px",
-                },
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-            >
-              {teacherInfo}
-            </Typography>
-          </Box>
-        </CardContent>
-        {user?.role === "admin" && (
-          <Box
+          <CardMedia
+            component="img"
             sx={{
+              width: "76px",
+              height: "58px",
+              aspectRatio: "4 / 3",
+              objectFit: "cover",
+              borderRadius: 1,
+              border: "1px outset whitesmoke",
+              filter: "drop-shadow(0px 0.6px 0.6px rgba(0, 0, 0, 0.7))",
+            }}
+            image={imageSrc}
+            alt="Capa do curso"
+          />
+        </Box>
+
+        <Box
+          sx={{
+            marginLeft: -1.8,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <CardContent
+            sx={{
+              textAlign: "left",
               display: "flex",
-              justifyContent: "flex-end",
-              alignItems: "center",
-              marginLeft: "auto",
+              flexDirection: "row",
+              justifyContent: "space-between",
               width: "100%",
             }}
           >
-            <Box
-              component="img"
-              src={options}
-              sx={{
-                width: "24px",
-                mr: 2,
-                ":hover": { transform: "scale(1.2)" },
-              }}
-            />
-          </Box>
-        )}
-      </Box>
-    </Card>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Typography
+                sx={{
+                  fontSize: { xs: "15px", sm: "16px", md: "18px", lg: "20px" },
+                  fontWeight: 600,
+                  maxWidth: {
+                    xs: "180px",
+                    sm: "190px",
+                    md: "200px",
+                    lg: "100%",
+                  },
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                }}
+              >
+                {name}
+              </Typography>
+              <Typography
+                variant="subtitle2"
+                sx={{
+                  color: "text.secondary",
+                  fontSize: { xs: "12px", md: "14px", lg: "16px" },
+                  maxWidth: {
+                    xs: "140px",
+                    sm: "380px",
+                    md: "500px",
+                    lg: "540px",
+                  },
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                }}
+              >
+                {teacherInfo}
+              </Typography>
+            </Box>
+          </CardContent>
+
+          {user?.role === "admin" && (
+            <Box sx={{ display: "flex", alignItems: "center", ml: "auto" }}>
+              <Box
+                component="img"
+                src={options}
+                onClick={handleOpenMenu}
+                alt="Opções"
+                sx={{
+                  width: "24px",
+                  mr: 2,
+                  ":hover": { transform: "scale(1.2)" },
+                  cursor: "pointer",
+                }}
+              />
+            </Box>
+          )}
+        </Box>
+      </Card>
+
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleCloseMenu}
+      >
+        {adminMenu.map((option) => (
+          <MenuItem key={option} onClick={() => handleMenuClick(option)}>
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+
+      <Dialog
+        open={openProfileModal}
+        onClose={() => setOpenProfileModal(false)}
+      ></Dialog>
+    </>
   );
 }
