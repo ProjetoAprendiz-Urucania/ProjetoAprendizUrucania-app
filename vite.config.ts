6fe6a78bf1d29d6f0790c0c6d660805c2d291789
jvvaccari <jv_vaccari@live.com>
2025-04-12 15:39:36 -0300

    Atualiza dependência do Vite para a versão 6.2.6 e ajusta limite de aviso de tamanho de chunk para 1000.

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 500,
+    chunkSizeWarningLimit: 1000, 
     rollupOptions: {
       output: {
-       
         manualChunks: {
           vendor: ['react', 'react-dom'],
+          mui: ['@mui/material', '@mui/icons-material', '@mui/x-data-grid'],
         },
       },
     },
     sourcemap: false,
-    target: 'esnext',  
+    target: 'esnext',
   },
 })
