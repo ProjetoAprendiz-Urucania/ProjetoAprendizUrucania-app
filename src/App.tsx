4bf1a2a095ae1312bdd182bf3fb060fada7a994a
jvvaccari <jv_vaccari@live.com>
2025-05-02 12:37:44 -0300

    ajustando redirecionamento ao atualizar pagina de classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  useLocation,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./Theme/theme";
 import "./App.css";
@@ -11,15 +17,25 @@ import { LessonPage } from "./Pages/LessonPage";
 import ProtectedRoute from "./hoc/ProtectedRoute";
 import { useAuth } from "./hooks/useAuth";
 import { Footer } from "./components/Footer/Footer";
+import { useEffect } from "react";
 
 function App() {
   const location = useLocation();
   const { token, logout } = useAuth();
+  const navigate = useNavigate();
 
   const isLoginPage = location.pathname === "/login";
   const isRegisterPage = location.pathname === "/register";
   const isForgot = location.pathname === "/forgot";
   const isNewPassword = location.pathname.startsWith("/newPassword");
+
+  useEffect(() => {
+    if (location.pathname !== "/" && location.pathname.endsWith("/")) {
+      navigate(location.pathname.slice(0, -1) + location.search, {
+        replace: true,
+      });
+    }
+  }, [location, navigate]);
 
   return (
     <ThemeProvider theme={theme}>
