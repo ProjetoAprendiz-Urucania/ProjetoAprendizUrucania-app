4ad7b2341409569da61c5b0ccfe3b3d20a11d3f3
jvvaccari <jv_vaccari@live.com>
2025-05-23 23:43:55 -0300

    Corrigindo AppLayout para não exibir footer

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,15 +1,21 @@
+import { useLocation } from "react-router-dom";
 import { Navbar } from "../components/Navbar/Navbar";
 import { useAuth } from "../hooks/useAuth";
 import { Container } from "@mui/material";
 import { Footer } from "../components/Footer/Footer";
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
+  const location = useLocation();
   const { token, logout } = useAuth();
+
+  const authRoutes = ["/login", "/register", "/forgot", "/newPassword"];
+  const isAuthRoute = authRoutes.includes(location.pathname);
+
   return (
     <>
-      <Navbar token={token} logout={logout} />
+      {!isAuthRoute && <Navbar token={token} logout={logout} />}
       <Container maxWidth="xl">{children}</Container>
-      <Footer/>
+       {!isAuthRoute &&  <Footer/>}
     </>
   );
 }
