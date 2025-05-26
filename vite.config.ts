e5c7968ad260f211991bb7365e8233f37d473ffb
João Vaccari <jvvaccari1@gmail.com>
2025-05-26 16:40:40 -0300

    Adiciona configuração de alias e diretório de saída no Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -15,5 +16,11 @@ export default defineConfig({
     },
     sourcemap: false,
     target: "esnext",
+    outDir: "dist",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
